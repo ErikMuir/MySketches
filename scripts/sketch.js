14f7e493bdfa707b7d329503126fee515674ddee
Erik Muir <erikdmuir@gmail.com>
2017-02-21 21:12:36 -0600

    Added arrow key support

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,36 +1,79 @@
-var active;
+// declare global variables
+window.IsPaused;
+window.MyContainer;
+window.Sketch;
+window.PauseResume;
+window.Save;
+window.CopyrightYear;
+window.Sketches = [];
+window.CurrentSketchIndex;
 
 $(window).ready(function () {
+	// set global variables
+	window.IsPaused = false;
+	window.MyContainer = $('#my-container');
+	window.Sketch = document.getElementById('my-container').contentWindow;
+	window.PauseResume = $('#pause-resume');
+	window.Save = $('#save');
+	window.CopyrightYear = $('#copyright-year');
+
+	// set the copyright year
+	window.CopyrightYear.text(new Date().getFullYear());
 	
 	// set size of container
-	$('#myContainer').attr({ 
+	window.MyContainer.attr({ 
 		'width': (innerHeight - 128),
 		'height': (innerHeight - 128)
-	});
-	
-	// load default sketch on page load
-	$('#myContainer').attr('src', 'sketches/sketch1.html');
-	active = true;
+	});	
 	
 	// listener for clicking on thumbnails
 	$('.thumb').on('click', function () {
-		var id = $(this).attr('id');
-		$('#myContainer').attr('src', 'sketches/' + id + '.html');
+		changeSketch(parseInt($(this).data('index')));
 	});
+
+	$('.thumb').each(function() {
+		window.Sketches.push($(this).attr('id'));
+	});
+
+	// listener for arrow keys
+	window.onkeyup = function(e) {
+		var key = e.keyCode ? e.keyCode : e.which;
+		if (key === 37 && window.CurrentSketchIndex > 0) {
+			changeSketch(window.CurrentSketchIndex - 1);
+		} else if (key === 39 && window.CurrentSketchIndex < window.Sketches.length - 1) {
+			changeSketch(window.CurrentSketchIndex + 1);
+		}
+	}
 	
 	// listener for clicking on pause/resume
-	$('#pauseresume').on('click', function () {
-		if (active) {
-			document.getElementById('myContainer').contentWindow.noLoop();
-		} else {
-			document.getElementById('myContainer').contentWindow.loop();
-		}
-		active = !active;
+	window.PauseResume.on('click', function () {
+		try {
+			if (window.IsPaused) 
+				window.Sketch.loop(); 
+			else 
+				window.Sketch.noLoop();
+			window.IsPaused = !window.IsPaused;
+		} catch(e) {
+			alert('This functionality is out of order');
+		}		
 	});
 	
 	// listener for clicking on save
-	$('#save').on('click', function () {
-		document.getElementById('myContainer').contentWindow.save();
+	window.Save.on('click', function () {
+		try {
+			window.Sketch.save();
+		} catch(e) {
+			alert('This functionality is out of order');
+		}
 	});
 	
+	// load default sketch on page load
+	changeSketch(0);
 });
+
+var changeSketch = function(index) {
+	window.CurrentSketchIndex = index;
+	$('.thumb').removeClass('active');
+	window.MyContainer.attr('src', 'sketches/' + window.Sketches[index] + '.html');
+	$('#' + window.Sketches[index]).addClass('active');
+};
